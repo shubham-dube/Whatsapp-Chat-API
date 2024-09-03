aad6d9ba051f675fa0e31c3ecd599ce13f958315
Shubham mishra <142476256+Shubham53Mishra@users.noreply.github.com>
2024-09-04 02:20:13 +0530

    Create model.js

---
+++ model.js
@@ -0,0 +1,13 @@
+// models/Message.js
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    status: { type: String, required: true },
+    message: { type: String, required: true },
+    messageBody: { type: String, required: true },
+    sender_Number: { type: String, required: true }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;

2763434f3a6af6c018df6ca7a55fdbddbf155f5e
Shubham Dubey <itzshubhamofficial@gmail.com>
2024-09-15 18:22:49 +0530

    new send messages endpoint added and auto reply to the user message removed

--- index.js
+++ index.js
@@ -5,25 +5,21 @@ const axios = require("axios");
 const app = express();
 require('dotenv').config();
 const helmet = require('helmet');
+
 app.use(helmet());
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/webhook", webhook.WEBHOOK_CALLBACK);
+
+app.post("/webhook", webhook.WEBHOOK_EVENT_HANDLER);
+
+app.post("/sendMessage", webhook.SEND_MESSAGE);
+
+app.get("/", (req, res) => res.status(200).send("Webhook Server is Running Fine !"));
 
 const PORT = 8000;
 
 app.listen(PORT, () => {
     console.log(`Webhook Server is listening on port ${PORT}`);
 });
-
-// Verification for the callback URL from the dashboard
-app.get("/webhook", webhook.WEBHOOK_CALLBACK);
-
-// Handle incoming webhook events
-app.post("/webhook", webhook.WEBHOOK_EVENT_HANDLER);
-
-app.post("/sendMessage", webhook.SEND_MESSAGE);
-
-// Basic endpoint to confirm server is running
-app.get("/", (req, res) => res.status(200).send("Webhook Server is Running Fine !"));
